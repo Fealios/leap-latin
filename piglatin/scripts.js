009d1573f31e3d74ee92bcaa0ee0eecf45f8fcc4
Matthew Caswell and Melvin Gruschow <matthew@>
2017-01-18 13:17:09 -0800

    fix our bugs for first spec in our pig

--- piglatin/scripts.js
+++ piglatin/scripts.js
@@ -1,42 +1,22 @@
 function pigLatin(string){
-  // var splitArr = [];
-  // var vowel = ['a', 'e', 'i', 'o', 'u'];
-  // var withAy = "";
-  // splitArr = string.toLowerCase().split('');
-  // console.log(string);
-  // console.log(splitArr);
-  // for (i=0;i<string.length;i++){
-  //   if (vowel.includes(string[i])){
-  //     withAy = string.push('ay');
-  //     console.log(withAy);
-  //     // return withAy;
-  //   }
-  // }
-  // console.log(withAy);
-  return (string + 'ay');
+  var splitArr = string.toLowerCase().split('');
+  var vowel = ['a', 'e', 'i', 'o', 'u'];
+
+  for(i=0;i<string.length;i++){
+    // console.log('in for loop');
+    if (vowel.includes(splitArr[0])){
+    }
+  }
+  console.log(splitArr.join('')+"ay");
+
 }
 
 $(document).ready(function(){
   $('#form').submit(function(event){
     event.preventDefault();
     var str = $('#userInput').val();
-    console.log(str);
-    console.log(pigLatin(str));
 
-    // $('#output').text(result);
+    pigLatin(str);
 
   });
 });
-
-
-
-// function pigLatin(string) {
-//   if(!isNaN(string)){
-//     console.log("it's a number"); //end of if statement
-//   } else {
-//
-//
-//     // console.log(splitStr.join(''));
-//     // console.log("it's not a number");
-//   } // end of else statment
-// }; // end of pig latin function
