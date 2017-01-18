30a8fb67ce62afe8673d27cc8e82e597974227fd
Matthew Caswell and Melvin Gruschow <matthew@>
2017-01-18 14:38:04 -0800

    finished spec of pig latin converter

--- piglatin/scripts.js
+++ piglatin/scripts.js
@@ -1,17 +1,20 @@
 function pigLatin(string){
   var splitArr = string.toLowerCase().split('');
   var vowel = ['a', 'e', 'i', 'o', 'u'];
-
+  var finalResult;
     if (vowel.includes(splitArr[0])){
-    console.log(splitArr.join('')+"ay");
+    finalResult = (splitArr.join('')+"ay");
   }
     else if(!vowel.includes(splitArr[0])){
       console.log('starts with consonant');
         var consonant = splitArr[0];
         var newStr = (splitArr.join('')+consonant + 'ay');
-        console.log(newStr.slice(1,newStr.length));
+        finalResult = (newStr.slice(1,newStr.length));
 
     } // end of else if statement
+
+  $('#result').text(finalResult);
+  // return finalResult;
 }
 
 $(document).ready(function(){
