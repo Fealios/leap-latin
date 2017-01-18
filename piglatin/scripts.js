b684eac72b357c6bf5ce9ef7b835b0f94b198be9
Matthew Caswell and Melvin Gruschow <matthew@>
2017-01-18 15:12:02 -0800

    add third spec to pig latin converter

--- piglatin/scripts.js
+++ piglatin/scripts.js
@@ -6,12 +6,12 @@ function pigLatin(string){
     finalResult = (splitArr.join('')+"ay");
   }
     else if(!vowel.includes(splitArr[0])){
-      console.log('starts with consonant');
-        var consonant = splitArr[0];
-        var newStr = (splitArr.join('')+consonant + 'ay');
-        finalResult = (newStr.slice(1,newStr.length));
-
+      var firstLetters = (string.match(/^[^aeiou]+/gi)).toString();
+      var total = string+firstLetters;
+      finalResult = (total.slice(firstLetters.length)+'ay');
     } // end of else if statement
+
+
 
   $('#result').text(finalResult);
   // return finalResult;
